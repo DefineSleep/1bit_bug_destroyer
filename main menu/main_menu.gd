b6ddd0277d6250e9cc90523f5fcef664201cd366
DefineSleep <123536705+DefineSleep@users.noreply.github.com>
2024-10-28 23:13:58 -0400

    making menu

--- main menu/main_menu.gd
+++ main menu/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
-
+const UPGRADE_MENU = preload("res://main menu/upgrade_menu.tscn")
+const LEVEL_1 = preload("res://level_1.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,4 +15,8 @@ func _process(delta: float) -> void:
 
 
 func _on_debug_text_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_1.tscn")
+	get_tree().change_scene_to_packed(LEVEL_1)
+
+
+func _on_upgrades_text_pressed() -> void:
+	get_tree().change_scene_to_packed(UPGRADE_MENU)
