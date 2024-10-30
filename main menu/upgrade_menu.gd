c28e1828f838a77577d5440189b40156e6dcea46
DefineSleep <123536705+DefineSleep@users.noreply.github.com>
2024-10-29 22:22:39 -0400

    upgrade menu - skill 1
    finished the base of all skill levelup

--- main menu/upgrade_menu.gd
+++ main menu/upgrade_menu.gd
@@ -20,36 +20,47 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	skill_one_name = "HP:" + str(Global.player_data.player_max_hp)
+	# Health 
+
 	update_skill_1()
-	player_money_label.text = str(Global.player_data.player_money)
+	player_money_label.text = str(Global.player_data.player_money) # money label
+	
+	
+	
+	
 	#TEST
 	test_label.text = str(upgrade_price_list)
 	test_label_2.text = str(refund_list)
 	
 # -------------------
-
+# all skills
+# health
+# health per second / health daemon 
+# moement_speed
+# range
+# lifesteal
+# invincibility skill
+# more bullet , maybe press button and for 5 secconds , big bullet
 
 
 # -------------------
-# SKILL ONE 
+# SKILL ONE  / HEALTH
 # -------------------
-
-# skill name 
-
-@onready var skill_one_name
+var skill_one_name
 var upgrade_price_list : Array = [8,12,24,47,86,122] 
 var refund_list : Array = []
 
 func update_skill_1():
+	skill_one_name = "HP:" + str(Global.player_data.player_max_hp)
 	stat_name.text = skill_one_name
+
 	if upgrade_price_list.is_empty() == false:
 		price.text = str(upgrade_price_list[0])
 	else: price.text = "SOLDOUT"
 
 
 
-func skill_pruchase(_skill_price)-> void:
+func skill_pruchase(_skill_price:Array)-> void:
 	if upgrade_price_list.is_empty() == false:
 		Global.player_data.player_max_hp +=1
 		Global.player_data.player_money -= _skill_price[0]
@@ -57,17 +68,18 @@ func skill_pruchase(_skill_price)-> void:
 		_skill_price.remove_at(0)
 	else : 
 		printerr("ITS EMPTY")
+		
+		
+func skill_refund(_skill_price:Array)->void:
+	if refund_list.is_empty() == false:
+		Global.player_data.player_money += refund_list[0] # put money back into player
+		upgrade_price_list.insert(0,refund_list[0]) # put refund price from refund list to upgrade list
+		refund_list.remove_at(0) # remove 0 index
+	else: print("cant refund")
 
-		
-		
-func skill_refund()->void:
-	Global.player_data.player_money += refund_list[0]
-	
 
 func _on_minus_pressed() -> void:
-	skill_refund()
-
-
+	skill_refund(upgrade_price_list)
 func _on_plus_pressed() -> void:
 	if upgrade_price_list.is_empty() == false :
 		if Global.player_data.player_money >= upgrade_price_list[0]:
@@ -76,3 +88,9 @@ func _on_plus_pressed() -> void:
 			plus.disabled = true
 			printerr("NOT ENOUGH MONEY HOE")
 	else: pass
+
+
+
+# -------------------
+# SKILL TWO MOVEMENT SPEED %
+# -------------------
